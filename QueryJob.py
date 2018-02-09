c9cd4844217ba7d2435f59874b25e1dde9fe9dad
Rupert Nash <rupert.nash@ed.ac.uk>
2018-02-09 09:50:49 +0000

    basic job status query classs

---
+++ QueryJob.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import os.path
+import time
+
+import azure.batch as batch
+
+from . import resources
+from .JobSpec import JobSpec
+from .BatchHelp import BatchHelper
+from .status import StatusReporter
+from .PrepareInput import InputPrepper
+
+class JobChecker(StatusReporter):
+    def __init__(self, group_name, batch_name, job_id, verbosity=1):
+        self.verbosity = verbosity
+        self.batch = BatchHelper(group_name, batch_name, verbosity=verbosity-1)
+        self.job_id = job_id
+
+    def wait_for_completion(self, timeout_s=None):
+        t0 = time.time()
+        tMax = t0 + timeout_s
+        while self.get_state() != batch.models.JobState.completed:
+            if time.time() > tMax:
+                self.critical("Timed out!")
+                raise RunTimeError("Job checking timed out")
+            
+            self.debug("Job not complete")
+            time.sleep(10)
+
+    def get_state(self):
+        job_info = self.batch.client.job.get(self.job_id)
+           return job_info.state
