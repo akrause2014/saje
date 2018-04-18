b681484b8bb6dfc92bd51db45b6f3b1d3be47446
akrause2014 <a.krause@epcc.ed.ac.uk>
2018-04-18 11:50:00 +0100

    environment parameters for auth config

--- az/auth.py
+++ az/auth.py
@@ -20,32 +20,41 @@ class Auth(object):
     ]
 }
 
-    Default location is ~/.azure/saje.json but you can override by 
-    setting SAJE_AUTH_CONFIG in your environment or supply it to 
+    Default location is ~/.azure/saje.json but you can override by
+    setting SAJE_AUTH_CONFIG in your environment or supply it to
     the constructor.
     '''
-    
+
     default_config = os.path.expanduser('~/.azure/saje.json')
     login_endpoint = 'https://login.microsoftonline.com/'
-    
+
     def __init__(self, name='default', config_path=None):
         if config_path is None:
             config_path = os.environ.get('SAJE_AUTH_CONFIG', self.default_config)
-            
-        with open(config_path) as cf:
-            config = json.load(cf)
-            
-        all_creds = {c['name']: c for c in config['credentials']}
-        cred = all_creds[name]
 
-        self.tenant_id = str(cred['tenant_id'])
-        self.subscription_id = str(cred['subscription_id'])
-        self.client_id = str(cred['client_id'])
-        self.secret = str(cred['secret'])
+        if os.path.exists(config_path):
+            with open(config_path) as cf:
+                config = json.load(cf)
+
+            all_creds = {c['name']: c for c in config['credentials']}
+            cred = all_creds[name]
+
+            self.tenant_id = str(cred['tenant_id'])
+            self.subscription_id = str(cred['subscription_id'])
+            self.client_id = str(cred['client_id'])
+            self.secret = str(cred['secret'])
+        else:
+            self.tenant_id = os.environ.get('SAJE_TENANT_ID')
+            self.subscription_id = os.environ.get('SAJE_SUBSCRIPTION_ID')
+            self.client_id = os.environ.get('SAJE_CLIENT_ID')
+            self.secret = os.environ.get('SAJE_SECRET')
+            if self.tenant_id is None or self.subscription_id is None \
+                or self.client_id is None or self.secret is None:
+                    raise Exception("No Azure auth config found")
 
         self.context = adal.AuthenticationContext(self.login_endpoint + self.tenant_id)
         self._resource_credentials = {}
-    
+
     def GetCredentialsForResource(self, resource):
         try:
             return self._resource_credentials[resource]
@@ -53,7 +62,7 @@ class Auth(object):
             ans = AdalAuthentication(self.context.acquire_token_with_client_credentials,
                                      resource, self.client_id, self.secret)
             self._resource_credentials[resource] = ans
-            return ans        
+            return ans
     @property
     def ManagementCredentials(self):
         return self.GetCredentialsForResource('https://management.azure.com/')
@@ -62,17 +71,17 @@ class Auth(object):
     def ResourceManagementClient(self):
         from azure.mgmt.resource import ResourceManagementClient
         return ResourceManagementClient(self.ManagementCredentials, self.subscription_id)
-    
+
     @cache
     def StorageManagementClient(self):
         from azure.mgmt.storage import StorageManagementClient
         return StorageManagementClient(self.ManagementCredentials, self.subscription_id)
-    
+
     @cache
     def ComputeManagementClient(self):
         from azure.mgmt.compute import ComputeManagementClient
         return ComputeManagementClient(self.ManagementCredentials, self.subscription_id)
-    
+
     @cache
     def BatchManagementClient(self):
         from azure.mgmt.batch import BatchManagementClient
@@ -85,7 +94,7 @@ class Auth(object):
     def NetworkManagementClient(self):
         from azure.mgmt.network import NetworkManagementClient
         return NetworkManagementClient(self.ManagementCredentials, self.subscription_id)
-    
+
     @cache
     def KeyVaultManagementClient(self):
         from azure.mgmt.keyvault import KeyVaultManagementClient
